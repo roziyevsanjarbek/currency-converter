31d42acabd64f9694dac84ef40b58c54d3749b67
Sanjarbek <roziyevsanjarbek1216@gmail.com>
2024-11-12 23:12:20 +0500

    Homework

--- currency.php
+++ currency.php
@@ -1,20 +1,35 @@
 <?php
 
-class Currency{
+class Currency {
 
-    const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/"
+    const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/"; // Nuqta-vergul qo'shildi
 
+    public array $currencies = [];    
 
-public function __construct(){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, CURRENCY_API_URL");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    public function __construct() {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::CURRENCY_API_URL); // Konstantani chaqirish `self::` orqali bo'ladi
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    $output = curl_exec($ch);
-    curl_close($ch);
-
-    $decoded = json_decode($output);
-    echo $decoded[0]->Ccy;
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $this->currencies = json_decode($output);
     }
 
+    public function getCurrencies(): array {
+        $separated_data = [];
+        $currencies_info = $this->currencies; 
+        foreach ($currencies_info as $currency) {
+            $separated_data[$currency->Ccy] = $currency->Rate;
+        }
+        return $separated_data;
+    }
+
+    public function exchange($value, $currency_name = 'USD') {
+        echo ceil($value / $this->getCurrencies()[$currency_name]) . ' ' . $currency_name;
+    }
 }
+
+
+$currency = new Currency();
+$currency->exchange(12800);
